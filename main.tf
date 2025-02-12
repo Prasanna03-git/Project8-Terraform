0f05c36528ff7a3452ec9707d053be63450d2917
Prasanna03-git <komakula3@gmail.com>
2025-02-11 21:23:47 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,10 @@ terraform{
     }
 }
 provider "azurerm" {
+  client_id       = "7c57baae-04c2-4a84-aabb-6eb2e0864265"
+  client_secret   = "2zL8Q~7cSWkA2J6ThS3t6db3bBq1B~qkTQibUakp"
+  tenant_id       = "a922897c-f8f9-429e-8fbf-dd631e8496f4"
+  subscription_id = "273dd228-606e-4083-a293-3c6b8d7062bd"
     features{
 
     }
